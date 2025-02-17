ba71643c8d6665a639584c506f548d491971f851
Kevin-Patyk <kvn.ptk@gmail.com>
2025-02-17 12:09:05 +0100

    feat: Complete z-score errors

--- crates/clamsform-core/src/feature_scaling/z_standardization/z_score.rs
+++ crates/clamsform-core/src/feature_scaling/z_standardization/z_score.rs
@@ -1,17 +1,17 @@
 use polars::prelude::*;
 
 use super::super::traits::FeatureScaler;
-use crate::utils::validation_errors::*;
 use super::z_standardization_errors::ZStandardizationError;
+use crate::utils::validation_errors::*;
 
 pub struct ZScoreTransformer {
     df: DataFrame,
-    mean: Option<DataFrame>,
-    std: Option<DataFrame>,
+    mean: Option<DataFrame,>,
+    std: Option<DataFrame,>,
 }
 
 impl ZScoreTransformer {
-    pub fn new(df: DataFrame) -> Self {
+    pub fn new(df: DataFrame,) -> Self {
         ZScoreTransformer {
             df,
             mean: None,
@@ -21,29 +21,29 @@ impl ZScoreTransformer {
 }
 
 impl FeatureScaler for ZScoreTransformer {
-    fn fit(&mut self) -> Result<(), ZStandardizationError> {
-        validate_dataframe(&self.df)?;
+    fn fit(&mut self,) -> Result<(), ZStandardizationError,> {
+        validate_dataframe(&self.df,)?;
 
-        self.mean = Some(self.df.clone().lazy().select([all().mean()]).collect()?);
-        self.std = Some(self.df.clone().lazy().select([all().std(1)]).collect()?);
+        self.mean = Some(self.df.clone().lazy().select([all().mean(),],).collect()?,);
+        self.std = Some(self.df.clone().lazy().select([all().std(1,),],).collect()?,);
 
-        Ok(())
+        Ok((),)
     }
 
-    fn transform(&self) -> Result<DataFrame, ZStandardizationError> {
-        validate_dataframe(&self.df)?;
+    fn transform(&self,) -> Result<DataFrame, ZStandardizationError,> {
+        validate_dataframe(&self.df,)?;
 
         let standardized_df = self
             .df
             .clone()
             .lazy()
-            .select([(all() - all().mean()) / all().std(1)])
+            .select([(all() - all().mean()) / all().std(1,),],)
             .collect()?;
 
-        Ok(standardized_df)
+        Ok(standardized_df,)
     }
 
-    fn fit_transform(&mut self) -> Result<DataFrame, ZStandardizationError> {
+    fn fit_transform(&mut self,) -> Result<DataFrame, ZStandardizationError,> {
         self.fit()?;
         self.transform()
     }
