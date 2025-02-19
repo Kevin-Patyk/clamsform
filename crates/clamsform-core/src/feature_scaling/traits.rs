bc2f6431f0d9dc9688c5f8f30f262a058d75a361
Kevin-Patyk <kvn.ptk@gmail.com>
2025-02-19 12:00:02 +0100

    feat: Finish implementing Z-score standardization

--- crates/clamsform-core/src/feature_scaling/traits.rs
+++
@@ -1,9 +0,0 @@
-use polars::prelude::DataFrame;
-
-use super::standardization::errors::ZStandardizationError;
-
-pub trait FeatureScaler {
-    fn fit(&mut self) -> Result<(), ZStandardizationError>;
-    fn transform(&self) -> Result<DataFrame, ZStandardizationError>;
-    fn fit_transform(&mut self) -> Result<DataFrame, ZStandardizationError>;
-}
